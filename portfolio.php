b170e989f570cc23b8961daa0e5773ab43aee0aa
Julien Faby <julienfaby@gmail.com>
2024-05-30 11:17:22 +0200

    mise à jour

--- portfolio.php
+++ portfolio.php
@@ -47,6 +47,9 @@
           </div>
         </header>
 
+
+        
+
       <section id="portfolio" class="portfolio">
         <div class="container">
           <div class="section-header">
@@ -54,7 +57,11 @@
           </div>
           <ul class="grid portfolio-filters list-unstyled">
           </ul>
+
+                    
+ <!-- Cartes projets--> 
           <div class="grid">
+        <!-- Carte projet n°1 -->     
             <div class="grid__item">
               <div class="card" data-category="web">
                 <img src="images\Garage1.png" loading="lazy" width="365" height="243"
@@ -83,7 +90,7 @@
                       <div class="grid__item">
                         <h4 class="modal__title h2">Garage V.Parrot</h4>
                         <p>
-                          <svg width="15" height="15" aria-hidden="true" focusable="false" data-prefix="far"
+                        <svg width="15" height="15" aria-hidden="true" focusable="false" data-prefix="far"
                             data-icon="calendar" class="svg-inline--fa fa-calendar fa-w-14" role="img"
                             xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512">
                             <path fill="currentColor"
@@ -93,13 +100,6 @@
                           <i>Année : 2023</i>
                         </p>
                         <p class="category">
-                          <svg width="15" height="15" aria-hidden="true" focusable="false" data-prefix="fas" data-icon="tag"
-                            class="svg-inline--fa fa-tag fa-w-16" role="img" xmlns="http://www.w3.org/2000/svg"
-                            viewBox="0 0 512 512">
-                            <path fill="currentColor"
-                              d="M0 252.118V48C0 21.49 21.49 0 48 0h204.118a48 48 0 0 1 33.941 14.059l211.882 211.882c18.745 18.745 18.745 49.137 0 67.882L293.823 497.941c-18.745 18.745-49.137 18.745-67.882 0L14.059 286.059A48 48 0 0 1 0 252.118zM112 64c-26.51 0-48 21.49-48 48s21.49 48 48 48 48-21.49 48-48-21.49-48-48-48z">
-                            </path>
-                          </svg>
                         <a href="https://github.com/Fabyjulien2A/Garage.git"><h5>Lien Github</h5></a>
                         <a href="https://www.garageecf.eu/"><h5>Lien site</h5></a>
                         </p>
@@ -123,9 +123,11 @@
                 </div>
               </div>
             </div>
+
+            <!-- Carte projet n°2 -->
             <div class="grid__item">
               <div class="card" data-category="design">
-                <img src="https://picsum.photos/600/400?random=2" loading="lazy" width="365" height="243"
+                <img src="images\images-tama\tama-1.png" loading="lazy" width="365" height="243"
                   alt="projet de la ville" class="card__image">
                 <div class="card__inner">
                   <h3 class="card__title">Tamagoshi</h3>
@@ -143,9 +145,9 @@
                   <div class="container">
                     <div class="grid">
                       <div class="grid__item">
-                        <img src="https://picsum.photos/600/400?random=2" loading="lazy" width="560" height="373"
+                        <img src="images\images-tama\tama-2.png" loading="lazy" width="560" height="373"
                           class="card__image" alt="Projet site de la ville de lyon" />
-                        <img src="https://picsum.photos/600/400?random=22" loading="lazy" width="560" height="373"
+                        <img src="images\images-tama\tama-3.png" loading="lazy" width="560" height="373"
                           class="card__image" alt="Projet site de la ville de lyon" />
                       </div>
                       <div class="grid__item">
@@ -161,13 +163,7 @@
                           <i>Année : 2024</i>
                         </p>
                         <p class="category">
-                          <svg width="15" height="15" aria-hidden="true" focusable="false" data-prefix="fas" data-icon="tag"
-                            class="svg-inline--fa fa-tag fa-w-16" role="img" xmlns="http://www.w3.org/2000/svg"
-                            viewBox="0 0 512 512">
-                            <path fill="currentColor"
-                              d="M0 252.118V48C0 21.49 21.49 0 48 0h204.118a48 48 0 0 1 33.941 14.059l211.882 211.882c18.745 18.745 18.745 49.137 0 67.882L293.823 497.941c-18.745 18.745-49.137 18.745-67.882 0L14.059 286.059A48 48 0 0 1 0 252.118zM112 64c-26.51 0-48 21.49-48 48s21.49 48 48 48 48-21.49 48-48-21.49-48-48-48z">
-                            </path>
-                          </svg>
+    
                           <a href="https://github.com/Fabyjulien2A/tama.git"><h5>Lien Github</h5></a>
                         </p>
                         <p>
@@ -191,13 +187,15 @@
                 </div>
               </div>
             </div>
+
+            <!-- Carte projet n°3 -->
             <div class="grid__item">
               <div class="card" data-category="app">
-                <img src="https://picsum.photos/600/400?random=3" loading="lazy" width="365" height="243"
+                <img src="images\burger-1.png" loading="lazy" width="365" height="243"
                   alt="projet de la ville" class="card__image">
                 <div class="card__inner">
-                  <h3 class="card__title">Titre du projet</h3>
-                  <p class="category">Applications</p>
+                  <h3 class="card__title">Landing page</h3>
+                  <p class="category">Landing page</p>
                 </div>
                 <div class="card__overlay">
                   <a href="#" class="card__link" data-id="modal-3">+</a>
@@ -211,13 +209,13 @@
                   <div class="container">
                     <div class="grid">
                       <div class="grid__item">
-                        <img src="https://picsum.photos/600/400?random=2" loading="lazy" width="560" height="373"
+                        <img src="images\burger-1.png" loading="lazy" width="560" height="373"
                           class="card__image" alt="Projet site de la ville de lyon" />
-                        <img src="https://picsum.photos/600/400?random=22" loading="lazy" width="560" height="373"
+                        <img src="images\burger-2.png" loading="lazy" width="560" height="373"
                           class="card__image" alt="Projet site de la ville de lyon" />
                       </div>
                       <div class="grid__item">
-                        <h4 class="modal__title h2">Titre de la modal 3</h4>
+                        <h4 class="modal__title h2">Landing page Burger house</h4>
                         <p>
                           <svg width="15" height="15" aria-hidden="true" focusable="false" data-prefix="far"
                             data-icon="calendar" class="svg-inline--fa fa-calendar fa-w-14" role="img"
@@ -226,7 +224,7 @@
                               d="M400 64h-48V12c0-6.6-5.4-12-12-12h-40c-6.6 0-12 5.4-12 12v52H160V12c0-6.6-5.4-12-12-12h-40c-6.6 0-12 5.4-12 12v52H48C21.5 64 0 85.5 0 112v352c0 26.5 21.5 48 48 48h352c26.5 0 48-21.5 48-48V112c0-26.5-21.5-48-48-48zm-6 400H54c-3.3 0-6-2.7-6-6V160h352v298c0 3.3-2.7 6-6 6z">
                             </path>
                           </svg>
-                          <i>Année : 2019</i>
+                          <i>Année : 2024</i>
                         </p>
                         <p class="category">
                           <svg width="15" height="15" aria-hidden="true" focusable="false" data-prefix="fas" data-icon="tag"
@@ -236,21 +234,11 @@
                               d="M0 252.118V48C0 21.49 21.49 0 48 0h204.118a48 48 0 0 1 33.941 14.059l211.882 211.882c18.745 18.745 18.745 49.137 0 67.882L293.823 497.941c-18.745 18.745-49.137 18.745-67.882 0L14.059 286.059A48 48 0 0 1 0 252.118zM112 64c-26.51 0-48 21.49-48 48s21.49 48 48 48 48-21.49 48-48-21.49-48-48-48z">
                             </path>
                           </svg>
-                          Refonte graphique
+                          <a href="https://github.com/Fabyjulien2A/faby-react"><h5>Lien Github</h5></a>
                         </p>
-                        <p>
-                          Lorem ipsum dolor sit amet consectetur adipisicing
-                          elit. Quaerat accusantium laborum aliquam enim. Vero
-                          voluptate, animi, sint vel mollitia ut odio, eius quis
-                          expedita quo at! Dignissimos ipsum odit dolores!
-                        </p>
-    
                         <h5 class="h3">Informations</h5>
                         <p>
-                          Lorem ipsum dolor sit amet consectetur adipisicing
-                          elit. Quaerat accusantium laborum aliquam enim. Vero
-                          voluptate, animi, sint vel mollitia ut odio, eius quis
-                          expedita quo at! Dignissimos ipsum odit dolores!
+                          Ce projet front end est une landing page créé avec le framework ReactJS
                         </p>
     
                         <h5 class="h3">Technologies</h5>
@@ -259,9 +247,6 @@
                           <li>HTML, CSS</li>
                           <li>Javascript</li>
                           <li>React</li>
-                          <li>Node.js</li>
-                          <li>Express.js</li>
-                          <li>MongoDB</li>
                         </ul>
                       </div>
                     </div>
@@ -269,6 +254,8 @@
                 </div>
               </div>
             </div>
+          
+            <!-- Carte projet n°4 -->
             <div class="grid__item">
               <div class="card" data-category="web">
                 <img src="https://picsum.photos/600/400?random=4" loading="lazy" width="365" height="243"
@@ -347,6 +334,8 @@
                 </div>
               </div>
             </div>
+
+            <!-- Carte projet n°5 -->
             <div class="grid__item">
               <div class="card" data-category="design">
                 <img src="https://picsum.photos/600/400?random=5" loading="lazy" width="365" height="243"
@@ -425,6 +414,8 @@
                 </div>
               </div>
             </div>
+
+            <!-- Carte projet n°6 -->
             <div class="grid__item">
               <div class="card" data-category="app">
                 <img src="https://picsum.photos/600/400?random=3" loading="lazy" width="365" height="243"
@@ -503,6 +494,7 @@
                 </div>
               </div>
             </div>
+
           </div>
         </div>
     
