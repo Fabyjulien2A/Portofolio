fce8f19db355e9e5454e11443ad0d4266e304c46
Faby <julienfaby@gmail.com>
2024-08-19 09:22:59 +0200

    mise à jour footer

--- index.php
+++ index.php
@@ -100,18 +100,9 @@
       
         </section>
        
-      
-        <footer>
-          <div class="container">
-            <div class="grid">
-              <div class="grid__item">
-              </div>
-              <div class="grid__item">
-                <p>Copyright © 2024.</p>
-              </div>
-            </div>
-          </div>
-        </footer>
+        <!--Footer -->
+        <?php require 'footer.php'; ?>
+
       
       </body>
       
