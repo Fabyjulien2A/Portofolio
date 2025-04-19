b796c68c5dc742bc1efb099e0096c1a6ac5957dd
Faby <julienfaby@gmail.com>
2025-04-19 21:03:36 +0200

    mise à jour

--- portfolio.php
+++ portfolio.php
@@ -115,10 +115,10 @@
                       <i>Année : 2023</i>
                     </p>
                     <p class="category">
-                      <a href="https://github.com/Fabyjulien2A/Garage.git">
+                      <a href="https://github.com/Fabyjulien2A/Garage.git" target="_blank">
                         <h5>Lien Github</h5>
                       </a>
-                      <a href="https://www.garageecf.eu/">
+                      <a href="https://www.garageecf.eu/" target="_blank">
                         <h5>Lien site</h5>
                       </a>
                     </p>
@@ -184,7 +184,7 @@
                     </p>
                     <p class="category">
 
-                      <a href="https://github.com/Fabyjulien2A/tama.git">
+                      <a href="https://github.com/Fabyjulien2A/tama.git" target="_blank">
                         <h5>Lien Github</h5>
                       </a>
                     </p>
@@ -256,7 +256,7 @@
                           d="M0 252.118V48C0 21.49 21.49 0 48 0h204.118a48 48 0 0 1 33.941 14.059l211.882 211.882c18.745 18.745 18.745 49.137 0 67.882L293.823 497.941c-18.745 18.745-49.137 18.745-67.882 0L14.059 286.059A48 48 0 0 1 0 252.118zM112 64c-26.51 0-48 21.49-48 48s21.49 48 48 48 48-21.49 48-48-21.49-48-48-48z">
                         </path>
                       </svg>
-                      <a href="https://github.com/Fabyjulien2A/faby-react">
+                      <a href="https://github.com/Fabyjulien2A/faby-react" target="_blank">
                         <h5>Lien Github</h5>
                       </a>
                     </p>
@@ -326,7 +326,7 @@
                           d="M0 252.118V48C0 21.49 21.49 0 48 0h204.118a48 48 0 0 1 33.941 14.059l211.882 211.882c18.745 18.745 18.745 49.137 0 67.882L293.823 497.941c-18.745 18.745-49.137 18.745-67.882 0L14.059 286.059A48 48 0 0 1 0 252.118zM112 64c-26.51 0-48 21.49-48 48s21.49 48 48 48 48-21.49 48-48-21.49-48-48-48z">
                         </path>
                       </svg>
-                      <a href="https://github.com/Fabyjulien2A/e-commerce">
+                      <a href="https://github.com/Fabyjulien2A/e-commerce" target="_blank">
                         <h5>Lien Github</h5>
                       </a>
                     </p>
@@ -399,7 +399,7 @@
                           d="M0 252.118V48C0 21.49 21.49 0 48 0h204.118a48 48 0 0 1 33.941 14.059l211.882 211.882c18.745 18.745 18.745 49.137 0 67.882L293.823 497.941c-18.745 18.745-49.137 18.745-67.882 0L14.059 286.059A48 48 0 0 1 0 252.118zM112 64c-26.51 0-48 21.49-48 48s21.49 48 48 48 48-21.49 48-48-21.49-48-48-48z">
                         </path>
                       </svg>
-                      <a href="https://github.com/Fabyjulien2A/plomberie-site">
+                      <a href="https://github.com/Fabyjulien2A/plomberie-site" target="_blank">
                         <h5>Lien Github</h5>
                       </a>
                     </p>
@@ -414,6 +414,7 @@
                       <li>HTML, CSS, Tailwind</li>
                       <li>Javascript</li>
                       <li>PHP (Laravel)</li>
+                      <li>MySQL</li>
                     </ul>
                   </div>
                 </div>
@@ -424,6 +425,80 @@
 
 
         <!-- Carte projet n°6 -->
+
+        <div class="grid__item">
+          <div class="card" data-category="app">
+            <img src="images/corsica-faby.png" loading="lazy" width="365" height="243" alt="projet de la ville"
+              class="card__image">
+            <div class="card__inner">
+              <h3 class="card__title">Site vitrine Corsica Faby Design</h3>
+              <p class="category">Site vitrine</p>
+            </div>
+            <div class="card__overlay">
+              <a href="#" class="card__link" data-id="modal-6">+</a>
+            </div>
+          </div>
+          <div class="modal" id="modal-6">
+            <button class="modal__close">&times;</button>
+
+            <div class="modal__content">
+
+              <div class="container">
+                <div class="grid">
+                  <div class="grid__item">
+                    <img src="images/faby-3.png" loading="lazy" width="560" height="373" class="card__image"
+                      alt="Projet site de la ville de lyon" />
+                    <img src="images/faby2.png" loading="lazy" width="560" height="373" class="card__image"
+                      alt="Projet site de la ville de lyon" />
+                  </div>
+                  <div class="grid__item">
+                    <h4 class="modal__title h2">Site vitrine Corsica Faby Design</h4>
+                    <p>
+                      <svg width="15" height="15" aria-hidden="true" focusable="false" data-prefix="far"
+                        data-icon="calendar" class="svg-inline--fa fa-calendar fa-w-14" role="img"
+                        xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512">
+                        <path fill="currentColor"
+                          d="M400 64h-48V12c0-6.6-5.4-12-12-12h-40c-6.6 0-12 5.4-12 12v52H160V12c0-6.6-5.4-12-12-12h-40c-6.6 0-12 5.4-12 12v52H48C21.5 64 0 85.5 0 112v352c0 26.5 21.5 48 48 48h352c26.5 0 48-21.5 48-48V112c0-26.5-21.5-48-48-48zm-6 400H54c-3.3 0-6-2.7-6-6V160h352v298c0 3.3-2.7 6-6 6z">
+                        </path>
+                      </svg>
+                      <i>Année : 2025</i>
+                    </p>
+                    <p class="category">
+                      <svg width="15" height="15" aria-hidden="true" focusable="false" data-prefix="fas" data-icon="tag"
+                        class="svg-inline--fa fa-tag fa-w-16" role="img" xmlns="http://www.w3.org/2000/svg"
+                        viewBox="0 0 512 512">
+                        <path fill="currentColor"
+                          d="M0 252.118V48C0 21.49 21.49 0 48 0h204.118a48 48 0 0 1 33.941 14.059l211.882 211.882c18.745 18.745 18.745 49.137 0 67.882L293.823 497.941c-18.745 18.745-49.137 18.745-67.882 0L14.059 286.059A48 48 0 0 1 0 252.118zM112 64c-26.51 0-48 21.49-48 48s21.49 48 48 48 48-21.49 48-48-21.49-48-48-48z">
+                        </path>
+                      </svg>
+                      <a href="https://github.com/Fabyjulien2A/corsica-faby-design"target="_blank">
+                        <h5>Lien Github</h5>
+                      </a>
+
+                      <a href="https://www.corsicafabydesign.fr" target="_blank">
+                        <h5>Lien site</h5>
+                      </a>
+
+                    </p>
+                    <h5 class="h3">Informations</h5>
+                    <p>
+                      Ce projet est un vitrine que j'ai réalisé avec le framework Laravel
+                    </p>
+
+                    <h5 class="h3">Technologies</h5>
+
+                    <ul>
+                      <li>HTML, CSS, Tailwind</li>
+                      <li>Javascript</li>
+                      <li>PHP (Laravel)</li>
+                      <li>MySQL</li>
+                    </ul>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
 
 
       </div>
